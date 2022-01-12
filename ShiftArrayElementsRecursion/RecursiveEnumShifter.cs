bf742069306694dd0b14735f3498c7e864363ce5
a91n0d <a91n0d@gmail.com>
2022-01-12 09:30:55 +0300

    Add project files.

---
+++ ShiftArrayElementsRecursion/RecursiveEnumShifter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace ShiftArrayElements
+{
+    public static class RecursiveEnumShifter
+    {
+        /// <summary>
+        /// Shifts elements in a <see cref="source"/> array using directions from <see cref="directions"/> array, one element shift per each direction array element.
+        /// </summary>
+        /// <param name="source">A source array.</param>
+        /// <param name="directions">An array with directions.</param>
+        /// <returns>An array with shifted elements.</returns>
+        /// <exception cref="ArgumentNullException">source array is null.</exception>
+        /// <exception cref="ArgumentNullException">directions array is null.</exception>
+        /// <exception cref="InvalidOperationException">direction array contains an element that is not <see cref="Direction.Left"/> or <see cref="Direction.Right"/>.</exception>
+        public static int[] Shift(int[] source, Direction[] directions)
+        {
+            // #1. Implement the method using recursive local functions and Array.Copy method.
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source), "Source not be null.");
+            }
+
+            if (directions is null)
+            {
+                throw new ArgumentNullException(nameof(directions), "Directions not be null.");
+            }
+
+            LeftSift(source, directions);
+            RightSift(source, directions);
+            return source;
+        }
+
+        /// <summary>
+        /// Shifts elements in a <see cref="source"/> array by left.
+        /// </summary>
+        /// <param name="source">>A source array.</param>
+        /// <param name="directions">An array with directions.</param>
+        private static void LeftSift(int[] source, Direction[] directions)
+        {
+            if (directions.Length < 1)
+            {
+                return;
+            }
+
+            if (directions[0] == Direction.Left)
+            {
+                int temp = source[0];
+                Array.Copy(source, 1, source, 0, source.Length - 1);
+                source[^1] = temp;
+                LeftSift(source, directions[1..]);
+            }
+            else if (directions[0] == Direction.Right)
+            {
+                LeftSift(source, directions[1..]);
+            }
+            else
+            {
+                throw new InvalidOperationException("Element of direction array contains invalid value.");
+            }
+        }
+
+        /// <summary>
+        /// Shifts elements in a <see cref="source"/> array by right.
+        /// </summary>
+        /// <param name="source">A source array.</param>
+        /// <param name="directions">An array with directions.</param>
+        private static void RightSift(int[] source, Direction[] directions)
+        {
+            if (directions.Length < 1)
+            {
+                return;
+            }
+
+            if (directions[0] == Direction.Right)
+            {
+                int temp = source[^1];
+                Array.Copy(source, 0, source, 1, source.Length - 1);
+                source[0] = temp;
+                RightSift(source, directions[1..]);
+            }
+            else if (directions[0] == Direction.Left)
+            {
+                RightSift(source, directions[1..]);
+            }
+            else
+            {
+                throw new InvalidOperationException("Element of direction array contains invalid value.");
+            }
+        }
+    }
+}
---
+++ ShiftArrayElementsRecursion/RecursiveShifter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace ShiftArrayElements
+{
+    public static class RecursiveShifter
+    {
+        /// <summary>
+        /// Shifts elements in a <see cref="source"/> array using <see cref="iterations"/> array for getting directions and iterations (odd elements - left direction, even elements - right direction).
+        /// </summary>
+        /// <param name="source">A source array.</param>
+        /// <param name="iterations">An array with iterations.</param>
+        /// <returns>An array with shifted elements.</returns>
+        /// <exception cref="ArgumentNullException">source array is null.</exception>
+        /// <exception cref="ArgumentNullException">iterations array is null.</exception>
+        public static int[] Shift(int[] source, int[] iterations)
+        {
+            // #2. Implement the method using recursive local functions and indexers only (don't use Array.Copy method here).
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source), "Source array not be null.");
+            }
+
+            if (iterations is null)
+            {
+                throw new ArgumentNullException(nameof(iterations), "Iterations array not be null.");
+            }
+
+            for (int i = 0; i < iterations.Length; i++)
+            {
+                while (iterations[i] != 0)
+                {
+                    ShiftLeft(source, source.Length - 1);
+                    iterations[i]--;
+                }
+
+                if (++i >= iterations.Length)
+                {
+                    break;
+                }
+
+                while (iterations[i] != 0)
+                {
+                    ShiftRight(source, 0);
+                    iterations[i]--;
+                }
+            }
+
+            return source;
+        }
+
+        /// <summary>
+        /// Shifts elements in a <see cref="source"/> array by right.
+        /// </summary>
+        /// <param name="source">A source array.</param>
+        /// <param name="start">Start index.</param>
+        public static void ShiftRight(int[] source, int start)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source), "Source array not be null.");
+            }
+
+            if (start >= source.Length - 1)
+            {
+                return;
+            }
+
+            (source[start], source[^1]) = (source[^1], source[start]);
+            ShiftRight(source, ++start);
+        }
+
+        /// <summary>
+        /// Shifts elements in a <see cref="source"/> array by left.
+        /// </summary>
+        /// <param name="source">A source array.</param>
+        /// <param name="start">Start index.</param>
+        public static void ShiftLeft(int[] source, int start)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source), "Source array not be null.");
+            }
+
+            if (start < 1)
+            {
+                return;
+            }
+
+            (source[start], source[0]) = (source[0], source[start]);
+            ShiftLeft(source, --start);
+        }
+    }
+}
